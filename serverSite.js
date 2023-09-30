dd690e4a21c8ff7117e31483e0b304862447496b
Moustiri abdelali <109457001+MoustiriAbdelali@users.noreply.github.com>
2023-09-30 19:10:47 +0100

    Update serverSite.js

--- serverSite.js
+++ serverSite.js
@@ -13,7 +13,37 @@ app.use( bodyParser.json())
 app.use(cors())
 
 
+app.post("/login", (req, res) => {
+    const sql = "SELECT  Clients.Host,Clients.bdd,Clients.UserName,Clients.passeword ,Tokens.id from Clients ,Tokens WHERE token=? and Tokens.IDClient=Clients.IDClient;";
+    db2.query(sql, [req.headers.authorization],(err,data_)=>{
+        const db3 = mysql.createConnection({
+            host: data_[0].Host ,
+            user: data_[0].UserName,
+            password: data_[0].passeword,
+            database: data_[0].bdd
+          });
+          const sql_ = "SELECT  Cuve.cuve_id FROM `Cuve` WHERE IDTokens=?";
+          db3.query(sql_, [data_[0].id],(err_,data__)=>{
 
+
+        console.log("-----------------------------------------------------------------------------------",req.body.volt);
+
+            const sqlInser="INSERT INTO `Mesur`( `date`, `Level`, `CuveCuveId`, `AlarmeLevel`, `AlarmeBattery`, `Volt`, `Rsrp`) VALUES (?,?,?,?,?,?,?)"
+            db3.query(sqlInser,[dateheur(),req.body.height  ,data__[0].cuve_id  ,req.body.empty_alarm,  req.body.battery_alarm  ,req.body.volt ,req.body.rsrp] ,(err___,date_____)=>{
+            
+            })
+
+
+
+
+
+
+     } )
+          });
+          
+
+return res.json("okk");
+    });
 
 
 
